7bfca1a8431e87c077539262b577815dc949ebf0
Chandra Mani Rai <78265746+moon-stone@users.noreply.github.com>
2025-04-05 16:51:33 +0530

    Time: 0 ms (100.00%) | Memory: 40.6 MB (95.73%) - LeetSync

--- 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
+++ 1993-sum-of-all-subset-xor-totals/sum-of-all-subset-xor-totals.java
@@ -1,10 +1,11 @@
 class Solution {
-
+    public int cal(int[] nums, int ind, int xor){
+        if(ind == nums.length) return xor;
+        int take = cal(nums, ind+1, xor^nums[ind]);
+        int notTake = cal(nums, ind+1, xor);
+        return take+notTake;
+    }
     public int subsetXORSum(int[] nums) {
-        int or = 0;
-        for(int i = 0; i < nums.length; i++){
-            or |= nums[i];
-        }
-        return or * (1 << (nums.length-1));
+        return cal(nums, 0, 0);
     }
 }
